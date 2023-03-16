4794ba9c25a92b8d8b1bf986d0829072fee1ce9e
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-03-16 02:02:17 -0300

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,6 +7,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('FuelBras')
+    .setDescription(
+      'Api criada para persistir os dados do sistema de cálculos do frontend.',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
